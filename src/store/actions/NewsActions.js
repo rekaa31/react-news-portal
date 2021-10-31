d3437649a0578556d0830e49fdbfa0d64481e0ed
Reka Alamsyah <reka.alamsyah@gmail.com>
2021-10-31 19:37:13 +0700

    (#) update action

--- src/store/actions/NewsActions.js
+++ src/store/actions/NewsActions.js
@@ -1,10 +1,5 @@
-// export const FETCH_NEWS = "FETCH_NEWS"
 export const ADD_BOOKMARK = "ADD_BOOKMARK"
 export const REMOVE_BOOKMARK = "REMOVE_BOOKMARK"
-
-// export const fetchNews = id => ({
-//     type: FETCH_NEWS,
-// });
 
 export const addBookmark = ({ id, title }) => ({
     type: ADD_BOOKMARK,
